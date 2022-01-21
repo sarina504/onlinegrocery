10009713f6f73a75d56075bfaacdc315cb829f89
sarina504 <74959780+sarina504@users.noreply.github.com>
2022-01-21 16:46:41 +0545

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,55 @@
+<div class="footer container-fluid bg-dark text-light">
+    <section class="footer">
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+                
+                    <div class="col l6 s12">
+            <a href="#" class="logo"><i class="fas fa-shopping-basket"></i>Online Grocery</a>
+
+            <p>Online Grocery is the project where the customer can order fruits and vegetables from using the internet website. This system, greatly simplifies the buying process for both the customer and the vendor. </p>
+             <div class="mt-2">
+                        <h5 class="small-title">Connect with Us</h5>     
+                    
+             <ul>
+                   <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                
+              
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                
+              
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                
+                </ul>
+            
+           </div>
+        </div>
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p> <i class="fas fa-home"></i>
+            tinkune,kathmandu
+        Nepal</p>
+                <p> <i class="fas fa-phone"></i>
+                    012345678
+                </p>
+                <p><i class="fas fa-envelope-square"></i>
+                    onlinegrocery12@gmail.com
+                </p>
+                 <p>
+
+<br><h5 class="small-title">Service Hour</h5>
+                        <span style="font-size:80%">08:00 AM to 7:00 PM </span>
+                    </p>
+              </div></div></div></footer></section></div>
+               
+         
+            <div class="container">
+              <h5 class="credit"><a class="" href="#!">  &#169<span>Online grocery 2021<i class="credit"></i></span> | all rights reserved! </a></h5>
+            
+            </div>
+          </div>
+
+        </footer>
+    </section>
+
+</div>
